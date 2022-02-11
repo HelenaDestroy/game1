e75578d304282edc0f1e386b0b14ae24c5e0fc23
jess <jesicatorti@gmail.com>
2022-02-11 15:00:00 -0300

    "Let there be chicken"

--- script.js
+++ script.js
@@ -8,6 +8,24 @@ fondo.objeto = new Image();
 fondo.objeto.src = fondo.url;
 fondo.objeto.addEventListener("load", cargarFondo);
 
+
+var cerdo = {
+    url: "cerdo.png",
+    cargaOk: false
+}
+cerdo.objeto = new Image();
+cerdo.objeto.src = cerdo.url;
+cerdo.objeto.addEventListener("load", cargarCerdos);
+
+var pollo = {
+    url: "pollo.png",
+    cargaOk: false
+}
+pollo.objeto = new Image();
+pollo.objeto.src = pollo.url;
+pollo.objeto.addEventListener("load", cargarPollos);
+
+
 var vaca = {   
     url: "vaca.png",
     cargaOk: false,
@@ -16,14 +34,23 @@ vaca.objeto = new Image();
 vaca.objeto.src = vaca.url;
 vaca.objeto.addEventListener("load", cargarVacas);
 
+function cargarFondo(){
+    fondo.cargaOk = true;
+    dibujar();
+    } 
 function cargarVacas(){
     vaca.cargaOk = true;
     dibujar();
 } 
-function cargarFondo(){
-fondo.cargaOk = true;
-dibujar();
+function cargarCerdos(){
+    cerdo.cargaOk = true;
+    dibujar();
 } 
+function cargarPollos(){
+    pollo.cargaOk = true;
+    dibujar();
+} 
+
 function dibujar()
 {
     if (fondo.cargaOk)
@@ -31,15 +58,30 @@ function dibujar()
         papel.drawImage (fondo.objeto, 0, 0);
     }
     if (vaca.cargaOk)
-    {
-        for (var i=0; i<10; i++) 
+    { 
+      var cantidad = aleatorio(5, 10);
+      for (var i=0; i<cantidad; i++) 
         {
             var x = aleatorio (0, 420);
             var y = aleatorio (0, 420);
-            papel.drawImage(vaca.objeto, x, y);
+            papel.drawImage (vaca.objeto, x, y);
+        }
+    }
+    if (cerdo.cargaOk)
+        {  
+            papel.drawImage(cerdo.objeto, 30, 30);
+        }
+    if (pollo.cargaOk)
+    {   var cantidad = aleatorio(5, 10);
+        for (var i=0; i<cantidad; i++) 
+        {
+        var x = aleatorio (0, 420);
+        var y = aleatorio (0, 420);
+        papel.drawImage(pollo.objeto, x, y);
         }
     }
 }
+
 
 
 function aleatorio(min,maxi)
@@ -47,20 +89,3 @@ function aleatorio(min,maxi)
     var resultado;
     resultado =  Math.floor(Math.random() * (maxi - min +1)) + min;
     return resultado;}
-
-// }
-// function dibujarCerdos()
-// {
-//     papel.drawImage(cerdo, 10, 300);
-// }
-// function dibujarPollos()
-// {
-//     papel.drawImage(pollo, 400, 150);
-// }
-
-// var cerdo = new Image();
-// cerdo.src = "cerdo.png";
-// cerdo.addEventListener("load", cargarCerdos);
-// var pollo = new Image();
-// pollo.src = "pollo.png";
-// pollo.addEventListener("load", cargarPollos);
